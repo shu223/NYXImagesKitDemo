f4f3cdd37ce744b4fbc80e5c7aeb3bf1086b0d1e
shu223 <shuichi0526@gmail.com>
2012-03-12 19:16:07 +0900

    Initial Commit

---
+++ NYXImageKitDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  NYXImageKitDemo
+//
+//  Created by 修一 堤 on 12/03/12.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ NYXImageKitDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NYXImageKitDemo
+//
+//  Created by 修一 堤 on 12/03/12.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
